3331c7594a4e081ea76cb06404e4ad2dffbd20db
Gabriel A. Amici <gabriel@amicinet.com>
2026-09-24 19:41:54 -0300

    Add factory methods to ClassicalSystem
    - Add from_energy factory method that creates an ensamble of
    trajectories of same energy, with positions distributed linearly
    from one turning point to the other;
    - Add from_angle factory method to create a similar ensamble with
    positions sampled from a linear distribution of angles from 0 to π.

--- src/emerald/classical/dynamics.py
+++ src/emerald/classical/dynamics.py
@@ -7,6 +7,7 @@ Base module for implementing Dynamics objects and Trajectory objects.
 """
 
 import numpy as np
+from scipy.interpolate import interp1d
 
 from ..fields.base import Field
 from ..integrators.base import Integrator
@@ -126,10 +127,106 @@ class ClassicalSystem:
             How many field periods the particles will be propagated for.
         """
         if self.field is None:
-            raise Exception("Field must be defined")
+            raise AssertionError("Field must be defined")
         t_eval = np.linspace(0, Nt*self.field.period, Nt)
         t_span = (t_eval[0], t_eval[-1])
 
         traj = self.propagate(integrator, t_span, t_eval)
 
         return PoincareMap(np.arange(Nt), traj.states, self.potential)
+
+    # Factory methods
+    @classmethod
+    def from_energy(cls, dynamics: Dynamics, E: float, N: int,
+                    momenta_signs: int = 0, position_signs: int = 0):
+
+        rm, rM = dynamics.potential.turning_points(E)
+
+        # Choose position interval
+        position_intervals = {
+            0:  (rm, rM),
+            1:  (0,  rM),
+            -1: (rm, 0),
+        }
+
+        try:
+            r0, r1 = position_intervals[position_signs]
+        except KeyError:
+            raise ValueError(
+                f"'position_signs' is {position_signs} but must be ±1 or 0"
+            )
+
+        if momenta_signs == 0:
+            pos = np.linspace(r0, r1, N // 2)
+            mom = dynamics.potential.momentum(E, pos)
+
+            states_plus  = np.column_stack((pos,  mom))
+            states_minus = np.column_stack((pos, -mom))
+            states0 = np.vstack((states_minus, states_plus))
+
+        elif abs(momenta_signs) == 1:
+            pos = np.linspace(r0, r1, N)
+            mom = dynamics.potential.momentum(E, pos)
+
+            states0 = np.column_stack((pos, momenta_signs * mom))
+
+        else:
+            raise ValueError(
+                f"'momenta_signs' is {momenta_signs} but must be ±1 or 0"
+            )
+
+        return cls(dynamics, states0)     
+
+    @classmethod
+    def from_angle(cls, dynamics: Dynamics, E: float, N: int,
+                   momenta_signs: int = 0, position_signs: int = 0):
+
+        rm, rM = dynamics.potential.turning_points(E)
+        theta0 = dynamics.potential.angle(E, 0)
+
+        pos_interval = np.linspace(rm, rM, 150)
+        ang_interval = dynamics.potential.angle(E, pos_interval)
+        interpolator = interp1d(ang_interval, 
+                                pos_interval, 
+                                kind='cubic', 
+                                fill_value="extrapolate")
+        
+        # Choose position interval
+        angle_intervals = {
+            0:  (0, np.pi),
+            1:  (theta0,  np.pi),
+            -1: (0, theta0),
+        }
+
+        try:
+            t0, t1 = angle_intervals[position_signs]
+        except KeyError:
+            raise ValueError(
+                f"'position_signs' is {position_signs} but must be ±1 or 0"
+            )
+
+
+        if momenta_signs == 0:
+            angs = np.linspace(t0, t1, N//2)
+
+            pos = interpolator(angs)
+            mom = dynamics.potential.momentum(E, pos)
+
+            states_plus  = np.column_stack((pos,  mom))
+            states_minus = np.column_stack((pos, -mom))
+            states0 = np.vstack((states_minus, states_plus))
+
+        elif abs(momenta_signs) == 1:
+            angs = np.linspace(t0, t1, N)
+            
+            pos = interpolator(angs)
+            mom = dynamics.potential.momentum(E, pos)
+
+            states0 = np.column_stack((pos, momenta_signs * mom))
+
+        else:
+            raise ValueError(
+                f"'momenta_signs' is {momenta_signs} but must be ±1 or 0"
+            )
+
+        return cls(dynamics, states0)
